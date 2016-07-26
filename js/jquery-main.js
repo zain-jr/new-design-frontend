00506853ede8e16d851f69c055a0ce2ec0a4f468
zain-jr <zainjrtech21@gmail.com>
2016-07-26 18:08:09 +0500

    day end

--- js/jquery-main.js
+++ js/jquery-main.js
@@ -149,6 +149,110 @@ $(document).on('click', '.extra-features', function(){
 	$('.list-extraFeatures').slideToggle();
 });
 
+$(document).on('keyup', '#search-society', function(){
+	var searchValue = $(this).val();
+	$('.societiesBlock-listing').find('li').each( function(){
+		var re = new RegExp(searchValue, 'gi');
+		 if($(this).text().match(re) == null){
+		 	$(this).hide();
+		 }else{
+			 $(this).show();
+		 }
+	});
+});
+
+$('.hidden-checkfield').change(function(){
+    if($(this).is(":checked")) {
+        $('.registration-form').addClass("agent-info");
+    } else {
+        $('.registration-form').removeClass("agent-info");
+		$('.company-logo').removeClass('hover');
+		$('.picture-holder').css({
+			'display':'none'
+		});
+    }
+});
+
+$(document).on('click', '.role-opener', function(){
+	$('.registration-form').find('.role-listing').slideToggle();
+	$(this).toggleClass('active');
+});
+
+function countCheckedRoles(){
+	var totalCheckedRoles = 0;
+	$('.userRole-checkbox').each(function() {
+  		if($(this).is(':checked'))
+		  totalCheckedRoles++;
+	});
+	if(totalCheckedRoles == 0)
+		$('.role-opener').html('Other Roles');
+	else	
+		$('.role-opener').html('Other Roles ( '+totalCheckedRoles+' Selected )');
+}
+
+$(document).on('change', '.userRole-checkbox', function(){
+	countCheckedRoles();
+});
+
+function countSelectedSocieties(){
+	var totalSelectedSocieties = 0;
+	$('.selectSociety-checkbox').each( function(){
+		if($(this).is(':checked')){
+			totalSelectedSocieties++;	
+		}
+		if(totalSelectedSocieties > 1){
+			$('.calculatedSocieties').text(totalSelectedSocieties+' Societies Selected');			
+		}
+		else {
+			if(totalSelectedSocieties == 1){
+				$('.calculatedSocieties').text(totalSelectedSocieties+' Society Selected');
+			}
+			else{
+				$('.calculatedSocieties').text('');
+			}
+		}
+	})
+}
+
+$(document).on('change', '.selectSociety-checkbox', function(){
+	countSelectedSocieties();
+});
+
+$(document).on('change', '.agent-brokerCheckbox', function(){
+	if($(this).is(':checked')){
+		$('.agent-brokerCheckbox').each(function(){
+			$(this).prop('checked', true);
+		});
+		$('.registration-form').addClass('agent-info')
+	}
+	else {
+		$('.agent-brokerCheckbox').each(function(){
+			$(this).prop('checked', false);
+			$('.registration-form').removeClass('agent-info')
+		});
+	}
+	countCheckedRoles();
+});
+
+function companyLogoUploader(file, target)
+ {
+	previewFile(file, target);
+	$(file).closest('.company-logo').find('.picture-holder').css({
+		 'display':'block'
+	});
+	$(file).closest('.company-logo').addClass('hover');
+ }
+ 
+ $(document).on('click', '.delete', function(){
+	 $(this).closest('.company-logo').find('.company-profileP').attr('src', '');
+	 $(this).closest('.company-logo').find('.company-profileP').attr('alt', '');
+	 $(this).closest('.company-logo').removeClass('hover');
+	 $(this).closest('.company-logo').find('.picture-holder').css({
+		 'display':'none'
+	 });
+ });
+ 
+
 // page init
 jQuery(function(){
   	initCarousel();
