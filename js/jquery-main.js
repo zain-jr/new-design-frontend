89485d7693e2c3aa471af193f2c85ad3efa975f4
zain-jr <zainjrtech21@gmail.com>
2016-07-28 17:19:35 +0500

    page ready agent detail

--- js/jquery-main.js
+++ js/jquery-main.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   $(".js-example-basic-single").select2();
-
   if (screen.width < 768){
 		$('.call-agent-btn').each(function(){
 			var mobileNumber = $(this).attr('data-tel');
@@ -13,7 +12,6 @@ $(document).ready(function() {
 	$('.addPro-type:first').trigger('change');
 	$('.list-extraFeatures').slideUp();
 	$('.registration-form').find('.role-listing').hide();
-
 });
 
 // page init
@@ -127,6 +125,7 @@ function initCarousel() {
 		maskAutoSize: false,
 		stretchSlideToMask: true,
 		switchTime: 2000,
+		step:1,
 		animSpeed: 600
 	});
 }
