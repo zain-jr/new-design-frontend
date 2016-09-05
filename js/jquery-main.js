2198fc297f225ff83fe702369e9df400b7ab725f
zain-jr <zainjrtech21@gmail.com>
2016-09-05 17:23:13 +0500

    dayend

--- js/jquery-main.js
+++ js/jquery-main.js
@@ -226,7 +226,7 @@ function searchBtnFix()
 {
 	var asideFromLeft = $('#aside').position().left;
 	$('.filter-btn').css({
-		'left':asideFromLeft+15,
+		'left':asideFromLeft+13,
 		'width':$('#aside').width()
 	});
 }
